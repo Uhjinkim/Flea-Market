e3afee4e49e53901cb05334078e7b7b526a343c9
Uhjinkim <ironmanuj@naver.com>
2024-10-02 05:53:55 +0900

    server build

--- api/src/main/kotlin/com/annotation/flea/config/CorsMvcConfig.kt
+++ api/src/main/kotlin/com/annotation/flea/config/CorsMvcConfig.kt
@@ -1,14 +1,17 @@
 package com.annotation.flea.config
 
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
+@Configuration
 class CorsMvcConfig(
     @Value("\${cors.allowed-origins}")private val allowedOrigins: String
 ) : WebMvcConfigurer {
     override fun addCorsMappings(registry: CorsRegistry) {
         registry.addMapping("/**")
+            .exposedHeaders("Set-Cookie")
             .allowedOrigins("http://localhost:3000")
             .allowedOrigins(allowedOrigins)
     }
